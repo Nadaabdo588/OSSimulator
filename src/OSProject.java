2e14bf494086aded91488b5901d85959893b6723
AbdallahElsayed73 <abdallah.arafa@student.guc.edu.eg>
2021-06-04 14:07:39 +0200

    k

--- src/OSProject.java
+++ src/OSProject.java
@@ -49,26 +49,41 @@ public class OSProject {
 
     public static void add(String x, String y) {
 
-    }
-    public static void assign(String x, String y) {
-        String data = readFromMemo(y);
-        if(data==null)
-            writeToMemo(x,y);
-        else
-            writeToMemo(x,data);
-    }
-    public static void writeFile(String a, String b) {
+        int xVal = Integer.parseInt(readFromMemo(x));
+        int yVal = Integer.parseInt(readFromMemo(y));
+        writeToMemo(x, ""+(xVal+yVal));
+
+
 
     }
-    public static String readFile(String a) {
-        return "";
+    public static void assign(String x, String y) {
+
+        writeToMemo(x,readFromMemo(y));
+    }
+    public static void writeFile(String a, String b) throws IOException {
+        PrintWriter pw = new PrintWriter(new FileWriter(a+".txt"));
+        String bVal = readFromMemo(b);
+        pw.print(bVal);
+        pw.close();
+        pw.flush();
+    }
+    public static String readFile(String a) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(a+".txt"));
+        StringBuilder ans = new StringBuilder();
+        if(br.ready())ans.append(br.readLine());
+        while(br.ready())
+        {
+            ans.append("\n");
+            ans.append(br.readLine());
+        }
+        return ans.toString();
     }
     public static String readAnInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         return br.readLine();
     }
     public static String readFromMemo(String x){
-        return memo.get(x);
+        return memo.getOrDefault(x,x);
     }
     public static void writeToMemo(String x,String y){
         memo.put(x,y);
